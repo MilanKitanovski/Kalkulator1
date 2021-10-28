6fe9497ecc46838a8d5ac5c3faae41a611b4a98f
Milan Kitanovski <lemi.kitanovski@gmail.com>
2021-10-28 19:12:42 +0200

    implemented substraction

--- src/Kalkulator/Main.java
+++ src/Kalkulator/Main.java
@@ -27,6 +27,8 @@ public class Main {
 			int y = Integer.parseInt(b[2]);
 			if(b[1].equals("+")) {
 				addition(x, y);
+			}else if(b[1].equals("-")) {
+				substraction(x, y);
 			}else {
 				System.out.println("Nije podrzana operacija!");
 			}
@@ -35,5 +37,8 @@ public class Main {
 	/*static - staticka metoda - Kao global u cpp - da uvek mozes pristupiti njoj, u bilo kom delu projekta*/
 	public static void addition(int a,int b) {
 		System.out.println(a+b);
+	}
+	public static void substraction(int a,int b) {
+		System.out.println(a-b);
 	}
 }
